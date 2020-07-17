491b4ab45a1ad234e4c28307bfe1dca0613769f5
Raphael <rapha@Raphaels-iMac.local>
2020-07-17 12:43:22 -0300

    creating login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
-import { KeyboardAvoidingView, Platform } from 'react-native';
-import { Container } from './styles';
+import { Image, KeyboardAvoidingView, Platform,  } from 'react-native';
+import { FontAwesome as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import logo from '../../../assets/Logo.png';
+
+import { 
+  Container, 
+  ImgContainer, 
+  InputContainer,
+  Input, 
+  Button, 
+  ButtonIcon,
+  ButtonText, 
+  NewUserContainer,
+  NewUserText
+} from './styles';
 
 const Login: React.FC = () => {
+  const navigation = useNavigation();
+
+  const handleNavigateToList = () => {
+    navigation.navigate('List');
+  }
+
+  const handlenavigateToRegister = () => {
+    navigation.navigate('Register');
+  }
+
   return (
-    <KeyboardAvoidingView  style={{ flex: 1}} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+    <KeyboardAvoidingView  style={{ flex: 1, }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Container>
-        
+        <ImgContainer>
+          <Image source={logo}/>
+        </ImgContainer>
+        <InputContainer>
+          <Input placeholder=" Email" autoCorrect={false} >
+            
+          </Input>
+          <Input placeholder=" Senha" secureTextEntry={true}>
+
+          </Input>
+          <Button onPress={handleNavigateToList}>
+            <ButtonIcon>
+              <Icon name="sign-out"  color="#EFD9CE" size={24}/>
+            </ButtonIcon>
+            <ButtonText>Login</ButtonText>
+          </Button>
+          <NewUserContainer onPress={handlenavigateToRegister}>
+            <Icon name="user-plus" size={16} color="#8F3985"/>
+            <NewUserText>Cadastrar usuário</NewUserText>
+          </NewUserContainer>
+        </InputContainer>
       </Container>
     </KeyboardAvoidingView>
   );
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,5 +1,83 @@
 import styled from 'styled-components/native';
+import { Animated } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import Constants from 'expo-constants';
 
 export const Container = styled.View`
+  flex: 1;
+  padding: 0 20px;
+  padding-top: ${Constants.statusBarHeight + 10}px;
   background-color: #CEA2AC;
+`;
+
+export const ImgContainer = styled.View`
+  flex: 1;
+  /* background-color: #FFF; */
+  width: 100%;
+  margin-top: -100px;
+  height: 180px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const InputContainer = styled(Animated.View)`
+  /* background-color: #000; */
+  flex: 1;
+  margin-top: -150px;
+  height: 250px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Input = styled.TextInput`
+  width: 95%;
+  align-items: center;
+  border-radius: 8px;
+  color: #25283D;
+  height: 40px;
+  margin-bottom: 20px;
+  padding: 10px;
+  background-color: #F5E7E0;
+`;
+
+export const Button = styled(RectButton)`
+  width: 95%;
+  height: 50px;
+  flex-direction: row;
+  align-items: center;
+  overflow: hidden;
+  background-color: #8F3985;
+  border-radius: 8px;
+`;
+
+export const ButtonText = styled.Text`
+  flex: 1;
+  text-align: center;
+  left: -20px;
+  justify-content: center;
+  font-size: 16px;
+  color: #EFD9CE;
+`;
+
+export const ButtonIcon = styled.View`
+  height: 50px;
+  width: 50px;
+  border-radius: 8px;
+  background: rgba(0, 0, 0, 0.1);
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NewUserContainer = styled.TouchableOpacity`
+  margin-top: 10px;
+  left: -50px;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const NewUserText = styled.Text`
+  margin-left: 8px;
+  text-align: justify;
+  font-size: 16px;
+  color: #8F3985;
 `;
