1aae0a7e91e1faeab5396681cabb497cf71266d7
Raphael Costa <rafa.costa0@hotmail.com>
2020-07-17 21:15:54 -0300

    adding screen header configuration

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from './pages/Login';
+import Register from './pages/Register';
+import List from './pages/List';
 
 const AppStack = createStackNavigator();
 
@@ -14,10 +16,13 @@ const Routes: React.FC = () => {
         screenOptions={{
           cardStyle: {
             backgroundColor: "#CEA2AC",
-          }
+          },
+          headerShown: false,
         }}
       >
         <AppStack.Screen name="Login" component={Login}/>
+        <AppStack.Screen name="Register" component={Register}/>
+        <AppStack.Screen name="List" component={List}/>
       </AppStack.Navigator>
     </NavigationContainer>
       );
